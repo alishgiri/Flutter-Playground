d034d9b9202e4e13471c49d971432df1da9dffcc
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-06-07 15:16:16 +0545

    Create placeholder screens

---
+++ lib/design/bottom_nav_view/events_calendar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EventsCalendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Text('Events Calendar'),
+        ],
+      ),
+    );
+  }
+}
